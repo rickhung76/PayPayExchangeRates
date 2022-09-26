242036800842d31ddd1188c2f32258f04ed01bfa
rickhuang76 <rick.huang@ichef.com.tw>
2022-09-27 00:36:14 +0800

    Test: CurrencyViewModelTest

--- PayPayExchangeRatesTests/ViewController/Currency/CurrencyViewModelTest.swift
+++ PayPayExchangeRatesTests/ViewController/Currency/CurrencyViewModelTest.swift
@@ -6,30 +6,128 @@
 //
 
 import XCTest
+@testable import PayPayExchangeRates
 
 class CurrencyViewModelTest: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    class MockPersistanceStore: PersistanceStorable {
+        var exchangeRate: ExchangeRate?
+        
+        init(_ rate: [String: Double] = [:]) {
+            self.exchangeRate = ExchangeRate(
+                disclaimer: "",
+                license: "",
+                timestamp: 0,
+                base: "",
+                rates: rate)
         }
     }
+    
+    func test_pickerData() throws {
+        // Arrange
+        let rate: [String: Double] = ["A": 0, "B": 1, "C": 2]
+        let vm = CurrencyViewModel(store: MockPersistanceStore(rate))
+        // Act
+        let pickerData = vm.pickerData
+        // Asset
+        XCTAssertEqual(pickerData, ["A", "B", "C"])
+    }
+    
+    func test_exchangeCurrency_fromTWDToUSDnJPY() throws {
+        // Arrange
+        let rate: [String: Double] = [
+            "TWD": 31.5,
+            "USD": 1.0,
+            "JPY": 145.0
+        ]
+        let vm = CurrencyViewModel(
+            store: MockPersistanceStore(rate),
+            defaultCountry: "TWD"
+        )
+        let twd = 31.5
+        // Act
+        let usd = vm.exchangeCurrency(amount: twd, to: "USD")
+        let jpy = vm.exchangeCurrency(amount: twd, to: "JPY")
+        // Assert
+        XCTAssertEqual(usd, "1.00000")
+        XCTAssertEqual(jpy, "145.00000")
+    }
+    
+    func test_exchangeCurrency_findNoCountryShouldReturnNaN() throws {
+        // Arrange
+        let rate: [String: Double] = [
+            "TWD": 31.5
+        ]
+        let vm = CurrencyViewModel(
+            store: MockPersistanceStore(rate),
+            defaultCountry: "TWD"
+        )
+        let twd = 31.5
+        let noSuchCountry = "NoSuchCountry"
+        // Act
+        let nan = vm.exchangeCurrency(
+            amount: twd,
+            to: noSuchCountry
+        )
+        // Assert
+        XCTAssertEqual(nan, "NaN")
+    }
+    
+    func test_updateCurrencyCellViewModels_byChangeCountry() throws {
+        // Arrange
+        let rate: [String: Double] = [
+            "TWD": 31.5,
+            "USD": 1.0,
+            "JPY": 145.0
+        ]
+        let vm = CurrencyViewModel(
+            store: MockPersistanceStore(rate),
+            defaultCountry: "TWD",
+            defaultAmount: "0"
+        )
+        // Act
+        vm.pickedCountry = "USD"
+        // Assert
+        let cell0 = try XCTUnwrap(vm.currencyCellVM[safe: 0])
+        XCTAssertEqual(cell0.country, "JPY")
+        XCTAssertEqual(cell0.amount, "0.00000")
+        let cell1 = try XCTUnwrap(vm.currencyCellVM[safe: 1])
+        XCTAssertEqual(cell1.country, "TWD")
+        XCTAssertEqual(cell1.amount, "0.00000")
+        let cell2 = try XCTUnwrap(vm.currencyCellVM[safe: 2])
+        XCTAssertEqual(cell2.country, "USD")
+        XCTAssertEqual(cell2.amount, "0.00000")
+    }
+    
+    func test_updateCurrencyCellViewModels_byChangeAmount() throws {
+        // Arrange
+        let rate: [String: Double] = [
+            "TWD": 31.5,
+            "USD": 1.0,
+            "JPY": 145.0
+        ]
+        let vm = CurrencyViewModel(
+            store: MockPersistanceStore(rate),
+            defaultCountry: "USD",
+            defaultAmount: "0"
+        )
+        // Act
+        vm.amount = "1.0"
+        // Assert
+        let cell0 = try XCTUnwrap(vm.currencyCellVM[safe: 0])
+        XCTAssertEqual(cell0.country, "JPY")
+        XCTAssertEqual(cell0.amount, "145.00000")
+        let cell1 = try XCTUnwrap(vm.currencyCellVM[safe: 1])
+        XCTAssertEqual(cell1.country, "TWD")
+        XCTAssertEqual(cell1.amount, "31.50000")
+        let cell2 = try XCTUnwrap(vm.currencyCellVM[safe: 2])
+        XCTAssertEqual(cell2.country, "USD")
+        XCTAssertEqual(cell2.amount, "1.00000")
+    }
+}
 
+extension Collection {
+    subscript (safe index: Index) -> Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
 }
